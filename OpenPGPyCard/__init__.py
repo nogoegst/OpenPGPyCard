8b57d5bddd84ef7eb9f6794dedf46b7ec94271b5
Ivan Markin <mark-in@users.noreply.github.com>
2015-10-17 16:06:31 +0000

    Version bump

--- OpenPGPyCard/__init__.py
+++ OpenPGPyCard/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-__version__ = "0.0.1dev1"
+__version__ = "0.0.1.dev2"
 __author__ = "Ivan Markin"
 __contact__ = "mark-in@users.noreply.github.com"
 __url__ = "https://github.com/mark-in/OpenPGPyCard"
